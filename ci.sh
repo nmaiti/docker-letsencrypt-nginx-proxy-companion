827e51e705b50e954469b9e6992531d035d9db9e
Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>
2021-02-06 02:13:31 +0530

    Add more platforms
    Signed-off-by: Nabendu Maiti <1206581+nmaiti@users.noreply.github.com>

--- ci.sh
+++ ci.sh
@@ -1,32 +1,32 @@
-#!/bin/bash 
+#!/bin/bash
 
 ###################################################################
 # Script Name : ci.sh
-# 
+#
 # Description :
 #
 # Args :
 #
 # Creation Date : 28-11-2020
 # Last Modified :
-# 
-# Created By : Nabendu 
+#
+# Created By : Nabendu
 ###################################################################
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" != "acme.sh-pini" ]; then
   docker buildx build \
     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/ppc64le \
+    --platform=linux/386,linux/amd64,linux/arm/v7,linux/arm/v6,linux/arm64,linux/ppc64le,linux/s390x \
     .
   exit $?
 fi
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERID --password-stdin &> /dev/null
 TAG="${TRAVIS_TAG:-latest}"
 docker buildx build \
-     --progress plain \
-    --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/ppc64le \
-	-t $DOCKER_USERID/$DOCKER_REPO:$TAG \
-    --push \
-    .
+  --progress plain \
+  --platform=linux/386,linux/amd64,linux/arm/v7,linux/arm/v6,linux/arm64,linux/ppc64le,linux/s390x \
+  -t $DOCKER_USERID/$DOCKER_REPO:$TAG \
+  --push \
+  .
 
